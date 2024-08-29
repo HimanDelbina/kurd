4ab74d37e963c70e2dbf65a9dd4b3a544697d063
unknown <himandelbina@gmail.com>
2024-08-29 18:31:04 +0330

    edit db1

--- kurd/settings.py
+++ kurd/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         "NAME": "kurd_db",
         "USER": "postgres",
         "PASSWORD": "11193",
-        "HOST": "0.0.0.0",
+        "HOST": "127.0.0.1",
         "PORT": "5432",
     }
 }
